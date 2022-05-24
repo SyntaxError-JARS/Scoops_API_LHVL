2f66f0e7e81a930860155cf7b97991f8d3454aa2
levihoulton <levijhoulton@gmail.com>
2022-05-24 15:20:18 -0500

    Added interface for CRUD. Added DAO methods for creditcard, customer, and menu.

--- src/main/java/com/revature/scoops/daos/OrderDao.java
+++ src/main/java/com/revature/scoops/daos/OrderDao.java
@@ -1,4 +1,34 @@
 package com.revature.scoops.daos;
 
-public class OrderDao {
+import com.revature.scoops.models.Order;
+import com.revature.scoops.util.Crudable;
+
+import java.io.IOException;
+import java.util.List;
+
+public class OrderDao implements Crudable<Order> {
+    @Override
+    public Order create(Order newObject) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAll() throws IOException {
+        return null;
+    }
+
+    @Override
+    public Order findById(String id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Order updatedObj) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
 }
